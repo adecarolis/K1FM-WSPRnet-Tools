70e5a6aa2555f5f091220ce21cddbff3b22b63f6
Alain De Carolis <alain@alain.it>
2019-12-31 18:58:18 -0500

    Add wspr-to-kml

--- K1FM_WSPRnet_Tools/WSPRnet_to_kml.py
+++ K1FM_WSPRnet_Tools/WSPRnet_to_kml.py
@@ -1,31 +1,180 @@
+import os
+import sys
 import csv
 import time
+import gzip
+import shutil
 import simplekml
+import pycurl
+from dateutil.relativedelta import relativedelta
 from K1FM_WSPRnet_Tools import gridsquare_functions
 
 
-if __name__ == '__main__':
-    with open('test.csv', 'rb') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
+def gunzip_shutil(source_filepath, dest_filepath, block_size=65536):
+    ''' Expands a gzipped file '''
 
-        positions = {}
-        for row in spamreader:
-            callsign = row[6]
-            epoch = row[1]
-            locator_4letters = row[7]
-            locator = locator_4letters + callsign[3:5].lower()
-            datetime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(epoch)))
-            lat, lng = to_latlng(locator)
-            positions[locator] = (datetime, lat, lng)
-        
-        kml = simplekml.Kml()
-        style = simplekml.Style()
-        style.labelstyle.color = simplekml.Color.red  # Make the text red
-        style.labelstyle.scale = 2  # Make the text twice as big
-        style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png'
+    with gzip.open(source_filepath, 'rb') as s_file, \
+            open(dest_filepath, 'wb') as d_file:
+        shutil.copyfileobj(s_file, d_file, block_size)
 
-        for locator in positions:
-            pnt = kml.newpoint(name=positions[locator][0] + ' - ' + locator, coords=[(positions[locator][2], positions[locator][1])])
-            pnt.style = style
+def progress_bar(total, existing, upload_t, upload_d):
+    sys.stdout.write('{0:.1f}'.format(existing / (total + 1) * 100, end=''))
+    sys.stdout.flush()
+    sys.stdout.write('% ')
+    sys.stdout.write('\b\b\b\b\b\b\b\b')
+    sys.stdout.flush()
 
-        kml.save('test.kml') 
+
+def download_file(url):
+    ''' Download a given URL '''
+
+    path = '/tmp/' + os.path.basename(url)
+
+    c = pycurl.Curl()
+    c.setopt(pycurl.URL, url)
+    c.setopt(pycurl.FOLLOWLOCATION, 1)
+    c.setopt(pycurl.MAXREDIRS, 5)
+
+    # Setup writing
+    if os.path.exists(path):
+        f = open(path, "ab")
+        c.setopt(pycurl.RESUME_FROM, os.path.getsize(path))
+    else:
+        f = open(path, "wb")
+
+    c.setopt(pycurl.WRITEDATA, f)
+
+    #c.setopt(pycurl.VERBOSE, 1) 
+    c.setopt(pycurl.NOPROGRESS, 0)
+    c.setopt(pycurl.XFERINFOFUNCTION, progress_bar)
+    try:
+        c.perform()
+    except:
+        pass
+
+def expand_gzip(path):
+    ''' Expand in place a gzipped archive'''
+
+    csv_path = path[0:-3]
+    assert path[-3:] == '.gz'
+
+    if not os.path.exists(csv_path):
+        gunzip_shutil(path, csv_path)
+
+
+def extract_data(path, callsign, first_identifier, second_identifier):
+    ''' Extract postions from WSPR data '''
+
+    assert path[-4:] == '.csv'
+
+    res = []
+    locators = []
+
+    with open(path, 'r') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        for row in csvreader:
+
+            if (row[6] == callsign or (row[6][0] == first_identifier and row[6][2] == str(second_identifier))):
+                if (row[6] == callsign):
+                    locators.append(row[7])
+                    res.append(row)
+                else:
+                    if (row[7] in locators):
+                        res.append(row)
+    
+    return res
+
+def get_months_list(start_month, end_month):
+    ''' Returns the list of months between a start and e end month '''
+
+    assert end_month >= start_month
+
+    months = []
+    months.append(start_month)
+    while start_month < end_month:
+        start_month = start_month + relativedelta(months=1)
+        months.append(start_month)
+    
+    return months
+
+def get_files_list(months):
+    ''' Returns the list of files that need to be downloaded given a list of months '''
+
+    assert len(months) > 0
+
+    files = []
+    for m in months:
+        files.append('wsprspots-' + m.strftime("%Y-%m") + '.csv.gz')
+    
+    return files
+
+
+def generate_kml_data(wspr_data, output):
+    ''' 
+        Given a list of WSPR datapoints, generates a dictionary of locator positions
+        to be used to generate a KML file:
+        Eg.:
+        {
+            'FN30AS': (datetime, lat, lng)
+        }
+        FN30AS:
+    '''
+    
+    altitude_dict = {
+        '0': 500,
+        '3': 1000,
+        '7': 2000,
+        '10': 3000,
+        '13': 4000,
+        '17': 5000, 
+        '20': 6000,
+        '23': 7000,
+        '27': 8000,
+        '30': 9000,
+        '33': 10000,
+        '37': 11000,
+        '40': 12000,
+        '43': 13000,
+        '47': 14000,
+        '50': 15000,
+        '53': 16000,
+        '57': 17000,
+        '60': 18000
+    }
+
+    altitude = 0
+    for row in wspr_data:
+        callsign = row[6]
+        epoch = row[1]
+        locator_4letters = row[7]
+        locator = locator_4letters + callsign[3:5].lower()
+
+        # From regular WSPR frames we just use the Altitude
+        if row[6][0] != 'Q' and row[6][0] != '0':
+            altitude = altitude_dict[row[8]]
+            continue
+
+        datetime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(epoch)))
+        lat, lng = gridsquare_functions.to_latlng(locator)
+        output[locator] = (datetime, lat, lng, altitude)
+    
+    return output
+
+def save_kml_file(data, filename):
+    ''' Saves a dictionary of locator positions to a KML formatted file '''
+
+    kml = simplekml.Kml()
+    #style = simplekml.Style()
+
+
+    for locator in data:
+        kml.newpoint(name=data[locator][0] + ' - ' + locator, coords=[(data[locator][2],
+                                                                       data[locator][1],
+                                                                       data[locator][3])])
+        # pnt.camera.latitude = data[locator][2]
+        # pnt.camera.longitude = data[locator][1]
+        # pnt.camera.altitude = data[locator][3]
+        # pnt.camera.altitudemode = simplekml.AltitudeMode.relativetoground
+        # pnt.style = style
+
+    kml.save(filename + '.kml')
---
+++ wspr-to-kml.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+
+import argparse
+import os
+from datetime import datetime
+from K1FM_WSPRnet_Tools import WSPRnet_to_kml
+
+def year_month_type(arg_value):
+    try:
+        year_month = datetime.strptime(arg_value, '%Y-%m')
+    except:
+        raise argparse.ArgumentTypeError
+    return year_month
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='This utility takes data from WSPR archives and generates KML files for K1FM Picoballoon flights.')
+    parser.add_argument('callsign', metavar='callsign', type=str, help='balloon callsign')
+    parser.add_argument('first_identifier', metavar='first_identifier', type=str, choices=['Q', '0'], help='first identifier (Q or 0)')
+    parser.add_argument('second_identifier', metavar='second_identifier', type=int, choices=range(0, 10), help='second identifier (0 to 9)')
+    parser.add_argument('--start-month', type=year_month_type, dest='start_month', help='year-month when the flight begun (eg. 2019-09). Defaults to current month')
+    parser.add_argument('--end-month', type=year_month_type, dest='end_month', help='year-month when the flight ended (eg. 2019-11). Defaults to current month')
+    parser.add_argument('--skip-downloads', dest='skip_downloads', action='store_true')
+    parser.add_argument('--debug', dest='debug', action='store_true')
+    parser.add_argument('--cleanup', dest='cleanup', action='store_true')
+
+    args = parser.parse_args()
+    debug = args.debug
+
+    if not args.start_month:
+        args.start_month = datetime.now()
+
+    if args.end_month:
+        if args.end_month < args.start_month:
+            print('Error: end_month must be greater or equal to start_month')
+            exit(1)
+    else:
+        args.end_month = datetime.now()
+
+    if debug:
+        print('Command line arguments:')
+        print(args)
+    
+    # get the list of the necessary files
+    months = WSPRnet_to_kml.get_months_list(args.start_month, args.end_month)
+    files = WSPRnet_to_kml.get_files_list(months)
+
+    url_base = 'http://wsprnet.org/archive/'
+    path_base = '/tmp/'
+
+    if not args.skip_downloads:
+        # Download the necessary files first
+        for n in files:
+            url = url_base + n
+
+            if debug:
+                print('Downloading ', url)
+            
+            WSPRnet_to_kml.download_file(url)
+    
+    if not args.skip_downloads:
+        # Expand the downloaded file
+        for n in files:
+            path = os.path.join(path_base, n)
+
+            if debug:
+                print('Expanding ', path)
+            
+            WSPRnet_to_kml.expand_gzip(path)
+
+    # Extract data from the files
+    wspr_data = []
+    for n in files:
+        path = os.path.join(path_base, n)
+        csv_path = path[0:-3]
+
+        if debug:
+            print('Extracting data from: ', os.path.basename(csv_path))
+        
+        res = WSPRnet_to_kml.extract_data(csv_path, args.callsign, args.first_identifier, args.second_identifier)
+        
+        wspr_data.extend(res)
+
+
+    kml_data = {}
+    res = WSPRnet_to_kml.generate_kml_data(wspr_data, kml_data)
+    filename = args.callsign + '_' + '_'.join([ m.strftime("%Y-%m") for m in months ])
+    if debug:
+        print('Writing KML file: {}.kml'.format(filename))
+    WSPRnet_to_kml.save_kml_file(res, filename)
+
+    if args.cleanup:
+        # Cleanup temporary files
+        for n in files:
+            if debug:
+                print('Cleaning up temporary files')
+            path = os.path.join(path_base, n)
+            csv_path = path[0:-3]
+            os.remove(path)
+            os.remove(csv_path)
